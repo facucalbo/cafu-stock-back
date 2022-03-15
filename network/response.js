50ac166213f586122953db25718ddd252f6e4ac6
Facundo Calbo <facuucalbo@gmail.com>
2022-03-15 15:33:36 -0300

    back end for cafu stock app

---
+++ network/response.js
@@ -0,0 +1,12 @@
+exports.success = function ( req, res, message, status ) {
+    //podemos usar logs para visualizar los errores, por ejemplo, error con la conexion a la base de datos
+    //esa informacion no se la podemos dar al cliente, por eso la podemos poner aca como console.log.
+    res.status(status).send({
+        error: '',
+        body: message
+    });
+}
+
+exports.error = function ( req, res ) {
+    
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+
+// const router = require('./components/message/network');
+const router = require('./network/routes');
+
+const app = express();
+app.use( bodyParser.json() );
+
+// app.use( router );
+router(app);
+
+
+app.use( '/app', express.static('public'));
+
+app.listen(3000);
